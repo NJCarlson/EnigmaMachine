41044c94124b0a918490158aa9747c4b21ed9de7
Nicholas Carlson <nick@simetri.us>
2021-04-22 11:10:26 -0400

    added comments and brittany code as example.

--- Form1.cs
+++ Form1.cs
@@ -10,18 +10,70 @@ using System.Windows.Forms;
 
 namespace EnigmaMachine
 {
-    public partial class Form1 : Form
+    public partial class EnigmaMachine : Form
     {
-        public Form1()
+        public EnigmaMachine()
         {
             InitializeComponent();
+
+            //Set Starting Variables:
+            comboBox1.Items.Insert(0, "Brittany's Algorithm");
+
+            //to change which item is selected in the combo box:
+            comboBox1.SelectedIndex = 0;
+
+            //set the example key:
+            textBox1.Text = " [XZNLWEBGJHQDYVTKFUOMPCIASRxznlwebgjhqdyvtkfuompciasr";
+
+            //to add more options to the combo box:
+            //comboBox1.Items.Insert(1, "");
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        //ENCRYPTION:
+        private void button2_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "Hello World!";
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    {
+                        string alphabet = " [abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                        string key = textBox1.Text;
+                        string encrypted_message = richTextBox1.Text;
+                        string decrypted_message = "";
+
+                        for (int i = 0; i < encrypted_message.Length; i++)
+                        {
+                            int position = -1;
+
+                            for (int j = 0; j < key.Length; j++)
+                            {
+                                if (key[j] == encrypted_message[i]) 
+                                {
+                                    position = j;
+                                    break;
+                                }
+                            }
+
+                            if (position != -1)
+                            {
+                                char new_char = alphabet[position];
+                                decrypted_message += new_char;
+                            }
+                            else
+                            {
+                                decrypted_message += encrypted_message[i];
+                            }
+                        }
+
+                        richTextBox1.Text = decrypted_message;
+
+                    }
+                        break;
+                        default:
+                    break;
+                    
+            }
         }
-
-
     }
 }
+
